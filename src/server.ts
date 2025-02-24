b2be1486387c2ae795bbe51bb554f0c541a4c916
Hasanfx <imrshadow.fx@gmail.com>
2025-02-24 11:07:45 +0300

    + allowed Cors for all origins

--- src/server.ts
+++ src/server.ts
@@ -9,8 +9,17 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
 app.use(express.json());
+
+const corsOptions = {
+  origin: "*", // Allow all origins
+  methods: "GET,POST,PUT,DELETE", // Adjust allowed HTTP methods as needed
+  allowedHeaders: "Content-Type, Authorization", // Allow specific headers
+};
+app.use(cors());
+
+
+
 
 // Routes
 app.get("/", (req, res) => {
